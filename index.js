d3ab225228521a5c7fc7eb2a86332fd660ed8b4d
demopurpose42 <144388451+demopurpose42@users.noreply.github.com>
2023-09-07 15:51:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000
   
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Server is up and running on 5000 ...`);
 });
   
